21ef004d2cee2eef3f6a10e5731bc38b7725cf7f
akaneburyo <akaneburyo@gmail.com>
2021-03-07 08:37:50 +0900

    update: 周辺機器が存在しない場合の表示を追加しました

--- Battery/BatteryGauge.swift
+++ Battery/BatteryGauge.swift
@@ -8,51 +8,62 @@
 import SwiftUI
 
 struct BatteryGauge: View {
-    @State var device:Device
+    @State var device:Device?
     
     var body: some View {
-        ZStack {
-            
-            // Stroke color
-            let color: Color = {
-                if self.device.batteryLevel <= 0.2 {
-                    return Color.red
-                } else {
-                    return Color.green
-                }
-            }()
-            
-            // Background
-            Circle()
-                .stroke(lineWidth: 4)
-                .opacity(0.3)
-                .foregroundColor(Color.gray)
-                .padding(8)
-            
-            // Gauge
-            Circle()
-                .trim(from: 0.0, to: CGFloat(self.device.batteryLevel))
-                .stroke(style: StrokeStyle(lineWidth: 4, lineCap: .round, lineJoin: .round))
-                .foregroundColor(color)
-                .rotationEffect(Angle(degrees: -90))
-                .padding(8)
-            
-            // Symbol
-            Text(self.device.symbol())
-                .contextMenu(/*@START_MENU_TOKEN@*/ContextMenu(menuItems: {
-                    let percent = Int(self.device.batteryLevel * 100)
-                    let batterySymbol:String = {
-                        switch percent {
-                            case 0..<10: return "􀛪"
-                            case 10..<40: return "􀛩"
-                            default: return "􀛨"
+        Group {
+            if let device = self.device {
+                ZStack {
+                    // Stroke color
+                    let color: Color = {
+                        if device.batteryLevel <= 0.2 {
+                            return Color.red
+                        } else {
+                            return Color.green
                         }
                     }()
                     
-                    Text(self.device.name ?? "NaN")
-                    Text("\(percent)% \(batterySymbol)")
-                })
-            )
+                    // Background
+                    Circle()
+                        .stroke(lineWidth: 4)
+                        .opacity(0.3)
+                        .foregroundColor(Color.gray)
+                        .padding(8)
+                    
+                    // Gauge
+                    Circle()
+                        .trim(from: 0.0, to: CGFloat(device.batteryLevel))
+                        .stroke(style: StrokeStyle(lineWidth: 4, lineCap: .round, lineJoin: .round))
+                        .foregroundColor(color)
+                        .rotationEffect(Angle(degrees: -90))
+                        .padding(8)
+                    
+                    // Symbol
+                    Text(device.symbol())
+                        .contextMenu(/*@START_MENU_TOKEN@*/ContextMenu(menuItems: {
+                            if let percent = Int(device.batteryLevel * 100) {
+                                let batterySymbol:String = {
+                                    switch percent {
+                                        case 0..<10: return "􀛪"
+                                        case 10..<40: return "􀛩"
+                                        default: return "􀛨"
+                                    }
+                                }()
+                                Text(device.name ?? "NaN")
+                                Text("\(percent)% \(batterySymbol)")
+                            }else {
+                                Text("Device not found.")
+                            }
+                        })
+                    )
+                }
+            } else {
+                Text("􀅍")
+                    .contextMenu(/*@START_MENU_TOKEN@*/ContextMenu(menuItems: {
+                        Text("Device not found.")
+                    })
+                )
+            }
         }
     }
 }
--- Battery/ContentView.swift
+++ Battery/ContentView.swift
@@ -10,26 +10,33 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject var batteryManager = BatteryManager()
     
-    let sync = #selector(AppDelegate.syncAction(_:))
-    
+    private let elementSize:CGFloat = 48
     var body: some View {
         ZStack {
             Color.init(NSColor(white: 1, alpha: 0.8))
                 .edgesIgnoringSafeArea(.all)
                 .cornerRadius(5.0)
-            
-            HStack(alignment: .center, spacing: 4, content: {
-                ForEach(self.batteryManager.devices) { device in
-                    BatteryGauge(device: device)
-                        .frame(width: 48, height: 48, alignment: .center)
+            let devices = self.batteryManager.devices
+            Group {
+                if devices.isEmpty {
+                    BatteryGauge(device: nil)
+                        .frame(width: elementSize, height: elementSize, alignment: .center)
+                } else {
+                    HStack(alignment: .center, spacing: 4, content: {
+                        ForEach(devices) { device in
+                            BatteryGauge(device: device)
+                                .frame(width: elementSize, height: elementSize, alignment: .center)
+                        }
+                    })
                 }
-            }       )
-            .padding(.all, 4)
-            .padding(.leading, 8)
-            .padding(.trailing, 8)
-        }.onCommand(sync, perform: {
+            }
+        }
+        .padding(.all, 4)
+        .padding(.leading, 8)
+        .padding(.trailing, 8)
+        .onReceive(NotificationCenter.default.publisher(for: .reload)) { _ in
             self.batteryManager.sync()
-        })
+        }
     }
 }
 
